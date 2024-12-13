c8a0eee0d4e3be282052bb649cee501e20a87b80
jjdavenport <jordanjamesdavenport@gmail.com>
2024-12-13 16:51:08 +0000

    fix: light mode text

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -7,7 +7,7 @@ const Input = ({ onSubmit, username, setUsername }) => {
         className="flex gap-2 rounded-2xl bg-white py-2 pr-2 shadow-md ~sm/md:~pl-4/8 dark:bg-lightBlack dark:shadow-none"
       >
         <input
-          className={`${username === "Not found!" ? "text-red-600" : "text-darkWhite"} flex w-full bg-search bg-mobile bg-left bg-no-repeat focus:outline-none ~sm/md:~pl-8/10 ~sm/lg:~text-base/lg ~sm/lg:placeholder:~text-sm/lg dark:bg-lightBlack dark:placeholder:text-darkWhite`}
+          className={`${username === "Not found!" ? "text-red-600" : "text-darkBlue dark:text-darkWhite"} flex w-full bg-search bg-mobile bg-left bg-no-repeat focus:outline-none ~sm/md:~pl-8/10 ~sm/lg:~text-base/lg ~sm/lg:placeholder:~text-sm/lg dark:bg-lightBlack dark:placeholder:text-darkWhite`}
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           onClick={() => (username === "Not found!" ? setUsername("") : null)}
