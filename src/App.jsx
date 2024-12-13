dfd647323b7e66df8ef2ce80e65787afc9a7c6b2
jjdavenport <jordanjamesdavenport@gmail.com>
2024-12-13 16:19:30 +0000

    fix: error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,43 @@ import { useEffect, useState } from "react";
 function App() {
   const [username, setUsername] = useState("octocat");
   const [data, setData] = useState(null);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    fetchData();
-    setUsername("");
-  }, []);
 
   const fetchData = async () => {
     try {
       const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error();
+      }
       const result = await response.json();
       setData(result);
-    } catch (error) {
-      setError(true);
+    } catch {
+      setUsername("Not found!");
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     fetchData();
-    console.log(data, username);
   };
 
   return (
-    <>
-      <div className="flex h-full min-h-screen flex-col bg-darkWhite p-6 font-custom text-base text-grayBlue transition-colors duration-300 ease-in-out md:items-center md:justify-center dark:bg-black dark:text-darkWhite">
-        <div className="flex w-full flex-col gap-10 md:max-w-screen-sm lg:max-w-screen-md">
-          <Header />
-          <div className="flex flex-col ~sm/md:~gap-4/6">
-            <Input
-              error={error}
-              username={username}
-              setUsername={setUsername}
-              onSubmit={handleSubmit}
-            />
-            <Output data={data} />
-          </div>
+    <div className="flex h-full min-h-screen flex-col bg-darkWhite p-6 font-custom text-base text-grayBlue transition-colors duration-300 ease-in-out md:items-center md:justify-center dark:bg-black dark:text-darkWhite">
+      <div className="flex w-full flex-col gap-10 md:max-w-screen-sm lg:max-w-screen-md">
+        <Header />
+        <div className="flex flex-col ~sm/md:~gap-4/6">
+          <Input
+            username={username}
+            setUsername={setUsername}
+            onSubmit={handleSubmit}
+          />
+          <Output data={data} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,19 +1,19 @@
-const Input = ({ onSubmit, username, setUsername, error }) => {
+const Input = ({ onSubmit, username, setUsername }) => {
   return (
     <>
       <form
         noValidate
         onSubmit={onSubmit}
-        className="flex rounded-2xl bg-white py-2 pr-2 shadow-md ~sm/md:~pl-4/8 dark:bg-lightBlack dark:shadow-none"
+        className="flex gap-2 rounded-2xl bg-white py-2 pr-2 shadow-md ~sm/md:~pl-4/8 dark:bg-lightBlack dark:shadow-none"
       >
         <input
-          className="flex w-full bg-search bg-mobile bg-left bg-no-repeat focus:outline-none ~sm/md:~pl-8/10 ~sm/lg:~text-base/lg ~sm/lg:placeholder:~text-sm/lg dark:bg-lightBlack dark:placeholder:text-darkWhite"
+          className={`${username === "Not found!" ? "text-red-600" : "text-darkWhite"} flex w-full bg-search bg-mobile bg-left bg-no-repeat focus:outline-none ~sm/md:~pl-8/10 ~sm/lg:~text-base/lg ~sm/lg:placeholder:~text-sm/lg dark:bg-lightBlack dark:placeholder:text-darkWhite`}
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          onClick={() => (username === "Not found!" ? setUsername("") : null)}
           type="text"
           placeholder="Search Github username…"
         />
-        {error && <span>Not found</span>}
         <button
           className="rounded-xl bg-blue py-3 font-semibold text-white shadow-sm ~sm/md:~text-xs/base ~sm/md:~px-3/6 dark:shadow-none"
           type="submit"
